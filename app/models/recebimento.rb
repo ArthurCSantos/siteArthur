8c1162bef336e1169b5d5f695a422a1673dba0e4
ArthurCSantos <arthurcarvalhosantos@gmail.com>
2017-01-05 11:21:25 -0200

    Remove models que ainda não estão funcionando

--- app/models/recebimento.rb
+++
@@ -1,8 +0,0 @@
-class Recebimento
-include Mongoid::Document
-
-  field :descricao,         type:String, default:nil
-  field :valor, 			type:String, default:nil
-  field :data, 				type:Time,	 default:nil
-
-end
